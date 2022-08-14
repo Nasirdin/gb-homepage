d305ae74ecf155f525fe8b1185550df94a042234
Nasirdin <nasirdin.zhaparkulov@gmail.com>
2022-08-14 21:57:04 +0600

    update functions

--- pages/functions/src/js/main.js
+++ pages/functions/src/js/main.js
@@ -7,7 +7,7 @@ const bg = document.querySelector(".bg");
 const newArrow = document.createElement("span");
 const burger = document.getElementById("burger");
 const nav = document.getElementById("nav");
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
 langBtn.addEventListener("click", () => {
   langOptions.classList.toggle("dropdown__items_active");
@@ -24,6 +24,27 @@ langClose.addEventListener("click", () => {
 burger.addEventListener("click", () => {
   burger.classList.toggle("header__burger_active");
   nav.classList.toggle("header__nav_active");
-  bg.classList.toggle("bg__active")
-  body.classList.toggle("lock")
+  bg.classList.toggle("bg__active");
+  body.classList.toggle("lock");
 });
+
+const boxIcon = document.querySelectorAll(".box__icon");
+const advantageIcon = document.querySelectorAll(".advantage__icon");
+const advantageIconTwo = document.querySelectorAll(".advantage__icon-2");
+
+function classIconColor(el, items) {
+  el.addEventListener("mouseover", () => {
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.add("dark-icon");
+    }
+  });
+  el.addEventListener("mouseout", () => {
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.remove("dark-icon");
+    }
+  });
+}
+
+boxIcon.forEach((el) => classIconColor(el, boxIcon));
+advantageIcon.forEach((el) => classIconColor(el, advantageIcon));
+advantageIconTwo.forEach((el) => classIconColor(el, advantageIconTwo));
